d5c534c3afd5065dd30fca5c6b5284d866b1a1f8
AndreyNesterov <nest.ande@yandex.ru>
2022-12-03 01:46:23 +0300

    task_01 complite

--- task_01.py
+++ task_01.py
@@ -4,3 +4,12 @@
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 import my_func
 n = my_func.get_int('How many numbers will be entered? ')
+list_num, sum_num = [], 0
+while n > 0:
+    list_num.append(my_func.get_int('Enter a number: '))
+    n -= 1
+for i in range(1,len(list_num), 2 ):
+    sum_num += list_num[i]
+print(f'The resulting list: {list_num}')
+print(f'Sum of odd elements = {sum_num}')
+
